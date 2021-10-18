081fd224f9f5d46a9d999936c7b73f23be27f857
Levon Ritter <levong2112@gmail.com>
2021-10-18 12:37:01 -0400

    revised footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Typography } from "@mui/material";
-import styles from "../styles/Home.module.sass";
+import footerStyles from "../styles/Footer.module.sass";
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className={footerStyles.footer}>
       {/* <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
