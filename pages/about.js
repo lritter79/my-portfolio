6de2d055f5d415d17a24ea9e0537cc1c2b44844e
Levon Ritter <levong2112@gmail.com>
2021-10-19 16:20:05 -0400

    revised about

--- pages/about.js
+++ pages/about.js
@@ -7,12 +7,12 @@ const about = () => {
       <p className="title">About</p>
       <Typography>
         I learned front-end web design from scratch when I was put in charge of
-        editing the website of the non-profit I interned at. Coding became a fun
-        new, creative outlet for me. I enrolled in Tech Elevator to learn more
-        about full-stack web development. After completing the program, I got my
-        first developer role at Civil and Environmental Consultants, Inc. I am
-        passionate about using technology to make tools for arts management and
-        education organizations.
+        editing the website at the non-profit where I interned. Coding became a
+        fun new creative outlet for me. I enrolled in Tech Elevator to learn
+        more about full-stack web development. After completing the program, I
+        got my first developer role at Civil and Environmental Consultants, Inc.
+        I am passionate about using technology to make tools for arts management
+        and education organizations.
       </Typography>
     </div>
   );
