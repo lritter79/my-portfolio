53a63fd216e5285e8e6d6633689bb881d7fdf1e8
Levon Ritter <levong2112@gmail.com>
2021-10-22 13:44:59 -0400

    made recent project load via client side

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,46 @@ import { skills } from "../data/skillsArray";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import NesContainer from "../components/NesContainer";
+import useSWR from "swr";
 
-export const getStaticProps = async () => {
-  const res = await fetch(`https://api.github.com/users/lritter79/repos`);
-  const repos = await res.json();
-  let sortedRepos = repos.sort(
-    (a, b) => new Date(a.pushed_at) - new Date(b.pushed_at)
-  );
-  let mostRecentlyUpdated =
-    sortedRepos.length > 0 ? sortedRepos[sortedRepos.length - 1] : null;
-  //this is how we get static props
-  //props must return a JSON object
-  return { props: { mostRecentlyUpdated } };
-};
+//this is what fetches the most repo I've been woring on, but it's static, so it's based on the last time the portfolio was commited
+//so I'm commenting this out to switch to dynamically loading the most recent project
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://api.github.com/users/lritter79/repos`);
+//   const repos = await res.json();
+//   let sortedRepos = repos.sort(
+//     (a, b) => new Date(a.pushed_at) - new Date(b.pushed_at)
+//   );
+//   let mostRecentlyUpdated =
+//     sortedRepos.length > 0 ? sortedRepos[sortedRepos.length - 1] : null;
+//   //this is how we get static props
+//   //props must return a JSON object
+//   return { props: { mostRecentlyUpdated } };
+// };
 
-export default function Home({ mostRecentlyUpdated }) {
+export default function Home() {
   const [seconds, setSeconds] = useState(0);
+  const repoUrl = `https://api.github.com/users/lritter79/repos`;
+
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
+
+  async function FetchMostRecent(url) {
+    await sleep(3000);
+    const res = await fetch(url);
+    console.log(res);
+    const repos = await res.json();
+    console.log(repos);
+    //const test = await setTimeout(5000, () => console.log(repos));
+    let sortedRepos = repos.sort(
+      (a, b) => new Date(a.pushed_at) - new Date(b.pushed_at)
+    );
+    console.log(sortedRepos[sortedRepos.length - 1]);
+
+    return sortedRepos.length > 0 ? sortedRepos[sortedRepos.length - 1] : null;
+  }
+  const { data, error } = useSWR(repoUrl, FetchMostRecent, 3);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -36,24 +60,29 @@ export default function Home({ mostRecentlyUpdated }) {
     <NesContainer title="Hello">
       <h5>My name is Levon Ritter. I`m a full stack web developer</h5>
       <div>
-        <a
-          href={mostRecentlyUpdated.svn_url}
-          className={`nes-badge is-splited ${homeStyles.nesBadge}`}
-        >
-          <span className="is-dark">Current</span>
-          <span className="is-success">Project</span>
-        </a>
-        <progress
-          className="nes-progress is-success"
-          value={seconds * 10}
-          max="100"
-        >{`${seconds * 10}%`}</progress>
-
-        <div className="nes-container is-rounded is-dark">
-          <a href={mostRecentlyUpdated.svn_url}>
-            <p>{mostRecentlyUpdated.description}</p>
-          </a>
-        </div>
+        {(!data || error) && (
+          <progress
+            className="nes-progress is-success"
+            value={seconds * 10}
+            max="100"
+          >{`${seconds * 10}%`}</progress>
+        )}
+        {data && (
+          <>
+            <a
+              href={data.svn_url}
+              className={`nes-badge is-splited ${homeStyles.nesBadge}`}
+            >
+              <span className="is-dark">Current</span>
+              <span className="is-success">Project</span>
+            </a>
+            <div className="nes-container is-rounded is-dark">
+              <a href={data.svn_url}>
+                <p>{data.description}</p>
+              </a>
+            </div>
+          </>
+        )}
       </div>
     </NesContainer>
   );
