ad203aa2ee86c7f155fbdd89ad73d1c5d0a7c64a
Levon Ritter <lritter@automated-health.com>
2021-12-22 01:24:11 -0500

    revising starfield, getting there

--- components/StarComponents/EightBitStar.js
+++ components/StarComponents/EightBitStar.js
@@ -3,7 +3,6 @@ import starStyles from "../../styles/EightBitStar.module.sass";
 import { useSpring, animated } from "react-spring";
 
 const EightBitStar = ({ bgColor, delay }) => {
-  const [paused, setIsPaused] = useState(false);
   const [leftvw, setLeftvw] = useState(Math.random() * 97);
   const hasCompleted = useRef(false);
   // useEffect(() => {
@@ -21,21 +20,20 @@ const EightBitStar = ({ bgColor, delay }) => {
     delay: hasCompleted.current ? 0 : delay * Math.random() * 10000,
     config: { duration: 10000 },
     to: async (next, cancel) => {
-      await next({ top: "-10%", opacity: 1 });
+      await next({ top: "0%" });
     },
-    from: { top: "100%", opacity: 0, left: `${Math.random() * 97}vw` },
+    from: { top: "100%", left: `${Math.random() * 97}vw` },
   });
   const flickerStyles = useSpring({
     loop: { reverse: true },
     reset: true,
     config: { duration: 1000 },
-    to: { filter: "blur(3px)" },
-    from: { filter: "blur(2px)" },
+    to: { filter: "blur(2px)" },
+    from: { filter: "blur(1px)" },
   });
 
   return (
     <animated.div
-      onClick={() => setIsPaused(!paused)}
       className={starStyles.star}
       style={{
         backgroundColor: bgColor,
--- components/StarComponents/Starfield.js
+++ components/StarComponents/Starfield.js
@@ -2,14 +2,27 @@ import React from "react";
 import EightBitStar from "./EightBitStar";
 import { colorArr } from "../../data/colorArray";
 import { useTrail, animated } from "react-spring";
+import { useState } from "react";
+
 
 const Starfield = () => {
-  const trailSprings = useTrail(colorArr.length, {});
+
+  function createStarArray() {
+    let starArr = [];
+    for (let i = 1; i < 20; i++) {
+      const star = {color: 'green', xPosition: (i * 5), yPosition:(Math.random() * 100), isFlashing:((Math.random()) * 2 == 1)}
+      starArr.push(star);
+    }
+
+    return starArr;
+  }
+
+  const starArray = createStarArray();
 
   return (
     <>
-      {trailSprings.map((spring, i) => {
-        return <EightBitStar bgColor={colorArr[i]} key={i} delay={i} />;
+      {starArray.map((star, i) => {
+        return <EightBitStar bgColor={star.color} key={i} delay={i} />;
       })}
     </>
   );
