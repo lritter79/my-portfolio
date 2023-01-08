85068b78910684bd4c32917cd0ebb3b65992fa50
Levon Ritter <lritter1@ottertune.com>
2023-01-08 00:48:58 -0500

    styled button

--- components/GalagaComponents/ArcadeButton.js
+++ components/GalagaComponents/ArcadeButton.js
@@ -4,7 +4,7 @@ import styles from "../../styles/ArcadeButton.module.scss";
 
 function ArcadeButton() {
   return (
-    <button type='button' className={`${styles.pushSkeuo} ${styles.push}`}></button>
+    <button type='button' className={`${styles.pushSkeuo} ${styles.push} ${styles.red}`}></button>
   )
 }
 
