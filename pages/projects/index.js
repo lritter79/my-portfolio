72e5f2e7300fb7d2f55e054d9c7d3cfbc0893e85
Levon Ritter <lritter1@ottertune.com>
2023-04-22 13:33:01 -0400

    upgraded next version

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -7,15 +7,6 @@ import Project from "../../components/Project";
 const projects = () => {
   return (
     <NesContainer title="Projects">
-      {/*
-      <ul className="nes-list is-circle">
-        {portfolioArray.map((project, i) => (
-          <li style={{ wordBreak: "break-all" }} key={i}>
-            <Link href={`/projects/${project.id}`}>{project.name}</Link>
-          </li>
-        ))}
-      </ul>
-      */}
       {portfolioArray.map((project, i) => (
         <Project project={project} key={project.id} index={i}></Project>
       ))}
