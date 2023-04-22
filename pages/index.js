d34f349d35d96f589d64eba148577339c2295233
Levon Ritter <lritter1@ottertune.com>
2023-04-22 15:46:11 -0400

    readded index

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import homeStyles from "../styles/Home.module.sass";
+import { skillsArray } from "../data/technicalSkillsArray";
+import { useState, useEffect } from "react";
+import NesContainer from "../components/NesContainer";
+import useSWR from "swr";
+import { colorByClassArr } from "../data/colorArray";
+import Skills from "../components/Skills";
+import CustomProgressBar from "../components/CustomProgressBar/CustomProgressBar";
+
+export default function Home() {
+  const repoUrl = `https://api.github.com/users/lritter79/repos`;
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
+
+  async function FetchMostRecent(url) {
+    await sleep(3000);
+    const res = await fetch(url);
+    //console.log(res);
+    const repos = await res.json();
+    //console.log(repos);
+    //const test = await setTimeout(5000, () => console.log(repos));
+    let sortedRepos = repos
+          .filter(repo => repo?.description)
+          .sort(
+            (a, b) => new Date(a.pushed_at) - new Date(b.pushed_at)
+          );
+    //console.log(sortedRepos[sortedRepos.length - 1]);
+
+    return sortedRepos.length > 0 ? sortedRepos[sortedRepos.length - 1] : null;
+  }
+  const { data, error } = useSWR(repoUrl, FetchMostRecent, 3);
+
+
+  //console.log(mostRecentlyUpdated);
+  return (
+    <NesContainer title="Hello">
+      <h5>My name is Levon Ritter. I`m a full stack web developer</h5>
+      <div>
+        <div className={`nes-badge is-splited ${homeStyles.nesBadge}`}>
+          <span className="is-dark">Current</span>
+          <span className="is-success">Project</span>
+        </div>
+        {(!data || error) && (
+          <>
+            <p>Now Loading ...</p>
+            <CustomProgressBar />
+          </>
+        )}
+        {data && (
+          <div className="nes-container is-rounded is-dark">
+            <a href={data.svn_url}>
+              <p>{data.description}</p>
+            </a>
+          </div>
+        )}
+      </div>
+      <Skills />
+    </NesContainer>
+  );
+}
